f90a8840d081a08ab28f928a843eebc62c7c6501
robert <Robertcmlist@gmail.com>
2023-04-05 15:36:36 -0600

    updated methods on edit_post and delete_post to reflect both GET and POST methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, abort, request
+from flask import Flask, render_template, redirect, url_for, flash, abort
 from functools import wraps
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -192,7 +192,7 @@ def add_new_post():
     return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=['GET', 'POST'])
 @admin_only
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
@@ -215,10 +215,9 @@ def edit_post(post_id):
     return render_template("make-post.html", form=edit_form, logged_in=current_user.is_authenticated)
 
 
-@app.route("/delete")
+@app.route("/delete/<int:post_id>", methods=['GET', 'POST'])
 @admin_only
-def delete_post():
-    post_id = request.args.get('post_id')
+def delete_post(post_id):
     post_to_delete = BlogPost.query.get(post_id)
     db.session.delete(post_to_delete)
     db.session.commit()
